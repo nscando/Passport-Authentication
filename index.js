636065acd0fe8812e0561931cbbcec3f61858de0
nscando <nicolas.scandizzo@gmail.com>
2020-10-15 19:25:15 -0300

    PassportJS implementation

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+const { config } = require('./config/index');
+const moviesApi = require('./routes/movies.js');
+
+const { logErrors, errorHandler, wrapErrors } = require('./utils/middleware/errorHandlers');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
+
+
+
+//body parser middleware
+app.use(express.json());
+
+//routes
+moviesApi(app);
+
+//Catch 404
+app.use(notFoundHandler);
+
+//errors middleware
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
+
+
+app.listen(config.port, function () {
+     console.log(`Listening http://localhost:${config.port}`)
+})
---
+++ test/routes.movies.test.js
@@ -0,0 +1,47 @@
+const assert = require('assert')
+const proxyquire = require('proxyquire')
+
+const { moviesMock, MoviesServiceMock } = require('../utils/mocks/movies')
+
+const testServer = require('../utils/testServer')
+
+
+describe('routes - movies', function () {
+     const route = proxyquire('../routes/movies', {
+          '../services/movies': MoviesServiceMock
+     })
+
+     const request = testServer(route);
+
+     describe('GET /movies', function () {
+          it('should respond with status 200', function (done) {
+               request.get('/api/movies').expect(200, done)
+          })
+
+          it('should respond with the list of movies', function (done) {
+               request.get('/api/movies').end((err, res) => {
+                    assert.deepEqual(res.body, {
+                         data: moviesMock,
+                         message: 'movies listed'
+                    })
+                    done();
+               });
+          });
+     });
+
+     // describe.only('POST /movies', function () {
+     //      it('should respond with 201 status', function (done) {
+     //           request.post('api/movies').expect(201, done)
+     //      })
+     //      it('should respond with movie ID created', function (done) {
+     //           request.post('/api/movies').end((err, res) => {
+     //                assert.deepEqual(res.body, {
+     //                     data: moviesMock[0].id,
+     //                     message: 'movie created'
+     //                });
+     //                done();
+     //           })
+     //      })
+
+     // })
+})
